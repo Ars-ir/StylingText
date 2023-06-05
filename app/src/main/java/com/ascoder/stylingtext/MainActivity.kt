c003dd11ead071df56c1a3f665e96acdf1bcb180
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-05 08:35:14 +0700

    - Adding Box with modifier fillMaxSize for box full size on screen and background color with hex black transparent

--- app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
+++ app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
@@ -3,21 +3,31 @@ package com.ascoder.stylingtext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComponentContainer()
         }
     }
 }
 
 @Composable
 fun ComponentContainer() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFF101010))) {
 
+    }
 }
 
 @Preview(showBackground = true)
