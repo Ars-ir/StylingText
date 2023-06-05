15cec41d4d60feea5cec45ef0d32d1a2a2a2bc1f
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-05 09:02:36 +0700

    - Adding font family on text

--- app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
+++ app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
@@ -12,15 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
-
-    val fontFamily = FontFamily(
-        Font(R.font.lexend_thin, FontWeight.Thin)
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +29,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComponentContainer() {
+    val fontFamily = FontFamily(
+        Font(R.font.lexend_black, FontWeight.Black),
+        Font(R.font.lexend_bold, FontWeight.Bold),
+        Font(R.font.lexend_extra_bold, FontWeight.ExtraBold),
+        Font(R.font.lexend_extra_light, FontWeight.ExtraLight),
+        Font(R.font.lexend_light, FontWeight.Light),
+        Font(R.font.lexend_medium, FontWeight.Medium),
+        Font(R.font.lexend_regular, FontWeight.Normal),
+        Font(R.font.lexend_semi_bold, FontWeight.SemiBold),
+        Font(R.font.lexend_thin, FontWeight.Thin)
+    )
+
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color(0xFF101010))) {
         Text(
             text = "Jetpack Compose",
             color = Color.White,
-            fontSize = 30.sp
+            fontSize = 30.sp,
+            fontFamily = fontFamily,
         )
     }
 }
