a24b680fee3aa125f4164f6f42ca1cebccb50df5
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-05 08:47:46 +0700

    - Rename font and adding variable font family

--- app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
+++ app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
@@ -10,10 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+
+    val fontFamily = FontFamily(
+        Font(R.font.lexend_thin, FontWeight.Thin)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
