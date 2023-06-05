b4ff83c038c97dbae90b889159a99f89c12f0d23
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-05 09:21:48 +0700

    - Create builder Annotation function returning AnnotatedString for text

--- app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
+++ app/src/main/java/com/ascoder/stylingtext/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
@@ -47,7 +51,7 @@ fun ComponentContainer() {
         .fillMaxSize()
         .background(Color(0xFF101010))) {
         Text(
-            text = "Jetpack Compose",
+            text = builderAnnotation(),
             color = Color.White,
             fontSize = 30.sp,
             fontFamily = fontFamily,
@@ -56,6 +60,57 @@ fun ComponentContainer() {
             textAlign = TextAlign.Center,
             textDecoration = TextDecoration.Underline
         )
+    }
+}
+
+@Composable
+fun builderAnnotation(): AnnotatedString {
+    return buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                color = Color.Green,
+                fontSize = 30.sp
+            )
+        ) {
+            append("G")
+        }
+        append("ulity ")
+        withStyle(
+            style = SpanStyle(
+                color = Color.Green,
+                fontSize = 30.sp
+            )
+        ) {
+            append("O")
+        }
+        append("f ")
+        withStyle(
+            style = SpanStyle(
+                color = Color.Green,
+                fontSize = 30.sp
+            )
+        ) {
+            append("A")
+        }
+        append("ll ")
+        withStyle(
+            style = SpanStyle(
+                color = Color.Green,
+                fontSize = 30.sp
+            )
+        ) {
+            append("T")
+        }
+        append("he ")
+        withStyle(
+            style = SpanStyle(
+                color = Color.Green,
+                fontSize = 30.sp
+            )
+        ) {
+            append("G")
+        }
+        append("ood")
     }
 }
 
